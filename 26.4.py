b26bedd7b7e7d0ab46831a2339e39a643d0c9239
Atanerys <159020841+Atanerys@users.noreply.github.com>
2024-03-03 00:30:45 +0300

    Update 26.4.py

--- 26.4.py
+++ 26.4.py
@@ -1,11 +1,27 @@
 import random
-while
-y= str(random())
-h= str(random())
-k=int(y) +int(h)
-j=int(input(y"+"h "=" )))
-while j!=k:
-    if j==k :
+s=0
+g=0
+while g<=2:
+    try:
+        y= int(random.random()*100)
+        h= int(random.random()*100)
+        k=y +h
+        j=int(input(str(y) +"+" + str(h) + "=" ))
+    except ValueError:
+         print("Надо числа писать")
+         g+=1
+         continue
+    while j!=k and g<=2:
+        try:
+            print('не верно')
+            g+=1
+            if g<=2:
+                j=int(input(str(y) +"+" + str(h) + "=" ))
+        except ValueError:
+            print("Надо числа писать")
+            g += 1
+    if j==k:
         print('верно')
-    else:
-        print('не верно')
+        s+=1
+if g==3:
+    print("Игра окончена.Павильных ответов:"+ str(s))
