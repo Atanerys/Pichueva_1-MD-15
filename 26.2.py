c0ccc7c8b30d1b5fca998a0a7077d5bdae69d9a4
Atanerys <159020841+Atanerys@users.noreply.github.com>
2024-03-02 22:41:58 +0300

    Update 26.2.py

--- 26.2.py
+++ 26.2.py
@@ -1,7 +1,8 @@
 
 b=''
-while a!=stop:
-    a=input('введите слово или напишите stop чтобы закончить')
-    if a!=stop:
+a=''
+while a!='stop':
+    a=input('введите слово или напишите "stop" чтобы закончить: ')
+    if a!='stop':
         b= b+" "+a
 print(b)
