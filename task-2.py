430ca81806190f2500189431c8164e117e356f8b
Atanerys <159020841+Atanerys@users.noreply.github.com>
2024-03-25 01:48:15 +0300

    Update task-2.py

--- task-2.py
+++ task-2.py
@@ -1,11 +1,11 @@
-def kl(a):
+def di():
     try:
-        a=100/int(a)
+        n = float(input("Введите число для деления 100: "))
+        result = 100 / int(n)
+        print("Результат деления 100 на " + str(n) + " равен" +  str(result))
     except ValueError:
-         print("Надо числа писать")
-         continue
-         return a
+        print("Ошибка: Введено не число.")
+    except ZeroDivisionError:
+        print("Ошибка: Деление на ноль невозможно.")
 
-a = input("Введите число ")
-d=kl(a)
-print(d)
+di()
